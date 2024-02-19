d45792e5f41a9c233a7865887f6f121242595933
ilberdogan <omerilber.dogan@gmail.com>
2024-02-19 21:57:58 +0300

    Update LibProject.py

--- LibProject.py
+++ LibProject.py
@@ -3,48 +3,48 @@ from tkinter import messagebox
 
 class LibraryGUI:
 
-    def __init__(self,master):
-
-        self.master=master
-        self.lib=Library()
+    def __init__(self, master):
+        self.master = master
+        self.lib = Library()
         master.title("Library App")
 
-        self.label=Label(master,text="MENU")
+        self.label = Label(master, text="MENU")
         self.label.pack()
 
-        self.list_button=Button(master,text="List Books",command=self.list_books)
+        self.list_button = Button(master, text="List Books", command=self.list_books)
         self.list_button.pack()
 
-        self.add_button=Button(master,text="Add Book",command=self.add_book)
+        self.add_button = Button(master, text="Add Book", command=self.add_book)
         self.add_button.pack()
 
-        self.delete_botton=Button(master,text="Delete Book",command=self.delete_book)
-        self.delete_botton.pack()
+        self.delete_button = Button(master, text="Delete Book", command=self.delete_book)
+        self.delete_button.pack()
 
     def list_books(self):
-        self.lib.list_books()
-        messagebox.showinfo("Kitaplar")
-    
+        books = self.lib.list_books()
+        books_str = '\n'.join(books)
+        messagebox.showinfo("Kitaplar", books_str)
+
     def add_book(self): 
-        self.book_name =input("Enter book name:")
-        self.book_author =input("Enter author's name:")
-        self.book_year =input("Enter year of publication:")
-        self.book_nop =input("Enter number of pages:")
+        self.book_name = input("Enter book name:")
+        self.book_author = input("Enter author's name:")
+        self.book_year = input("Enter year of publication:")
+        self.book_nop = input("Enter number of pages:")
         self.lib.add_book(self.book_name, self.book_author, self.book_year, self.book_nop)
         messagebox.showinfo("Kitap Eklendi", "Kitap başarıyla eklendi.")     
 
     def delete_book(self):
-        book_name=input("Name of the book to be deleted: ")
+        book_name = input("Name of the book to be deleted: ")
         self.lib.delete_book(book_name)    
         messagebox.showinfo("Kitap Kaldırıldı", "Kitap başarıyla kaldırıldı.")                                                                                                                  
 
 class Library:
 
     def __init__(self):
-        self.dosya_exe="Kitaplar.txt"
+        self.dosya_exe = "Kitaplar.txt"
 
-    def open_file(self,mode):
-        self.file=open(self.dosya_exe,mode) 
+    def open_file(self, mode):
+        self.file = open(self.dosya_exe, mode) 
     
     def close_file(self):
         self.file.close()
@@ -58,18 +58,18 @@ class Library:
                 book_name, book_author = book_info[:2]
                 books_list.append(f"Book name: {book_name}, Book author: {book_author}")
         self.close_file()
-        return books_list                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-                                                                                                                                                                                                       
-    def add_book(self,book_name,book_author,book_year,book_nop):
+        return books_list
+
+    def add_book(self, book_name, book_author, book_year, book_nop):
         self.open_file("a+")
-        line=f"{book_name},{book_author},{book_year},{book_nop}\n"
+        line = f"{book_name},{book_author},{book_year},{book_nop}\n"
         self.file.write(line)
         self.close_file()
     
-    def delete_book(self,book_name):
+    def delete_book(self, book_name):
         self.open_file("r")
-        lines=self.file.readlines()
-        delete_lines=[]
+        lines = self.file.readlines()
+        delete_lines = []
         for line in lines:
             if book_name in line:
                 delete_lines.append(line)
@@ -79,7 +79,7 @@ class Library:
             if line not in delete_lines:
                 self.file.write(line)
         self.close_file()
-    
+
 def main():
     root = Tk()
     app = LibraryGUI(root)
